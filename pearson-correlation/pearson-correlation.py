2a6cd48462e4243f335339b7f8ee15f6ca0a2a34
Joshua Carter <joshtcarter0710@gmail.com>
2026-09-02 21:05:05 +1000

    Solve pearson-correlation

---
+++ pearson-correlation/pearson-correlation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def pearson_correlation(X):
+    X = np.asarray(X, dtype=float)
+    centered = X - np.mean(X, axis=0)
+    covariance = centered.T @ centered / (X.shape[0] - 1)
+    standard_deviation = np.sqrt(np.diag(covariance))
+    denominator = np.outer(standard_deviation, standard_deviation)
+    with np.errstate(divide="ignore", invalid="ignore"):
+        return covariance / denominator
